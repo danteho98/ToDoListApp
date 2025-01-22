79c25ef84c9562cdb67b941062adb57b67671a3c
kaili <kailinho198@gmail.com>
2025-01-22 16:33:30 +0800

    Add project files.

---
+++ ToDoList/ToDoDbContext/ToDoDbContext.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using ToDoList.Models;
+
+namespace ToDoList.ToDoDbContext
+{
+    public class ToDoDbContext : DbContext
+    {
+        public ToDoDbContext(DbContextOptions<ToDoDbContext> options) : base(options) { }
+
+        public DbSet<ToDoItem> ToDoItems { get; set; }
+    }
+}
