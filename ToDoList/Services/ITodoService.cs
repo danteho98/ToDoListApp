fc74cd8d2b789d7162f9ce550ca9b22c7d6c4896
kaili <kailinho198@gmail.com>
2025-05-19 21:25:47 +0800

    Added Services file and added ITodoService

---
+++ ToDoList/Services/ITodoService.cs
@@ -0,0 +1,14 @@
+﻿namespace ToDoList.Services
+{
+    public interface ITodoService
+    {
+        Task<IEnumerable<ToDoItem>> GetAllToDoAsync();
+        Task<ToDoItem?> GetTodayByIdAssync(int id);
+        Task<IEnumerable<TodoItem>> GetincompleteToDoAsync();
+        Task<IEnumerable<ToDoItem>> GetToDosByPriorityAsync(ToDoPriority priority);
+        Task AddToDoAsync(ToDoItem ToDoItem);
+        Task UpdateToDoAsync(ToDoItem toDoItem);
+        Task DeleteToDoAsync(int id);
+        Task<bool> ToDoExistAsync(int id);
+    }
+}
