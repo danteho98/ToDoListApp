2ac9964513455bc5fbe08a218307c95ac1079c22
kaili <kailinho198@gmail.com>
2025-05-22 15:15:54 +0800

    Added ToDoCreateViewModel

---
+++ ToDoList/ViewModels/ToDoCreateViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using ToDoList.Models;
+
+namespace ToDoList.ViewModels
+{
+    public class ToDoCreateViewModel
+    {
+        [Required]
+        [StringLength(100)]
+        public string Title { get; set; } = string.Empty;
+
+        public string? Description { get; set; }
+
+        [DisplayName("Due Date")]
+        [DataType(DataType.Date)]
+        public DateTime? DueDate { get; set; }
+
+        public Priority Priority { get; set; } = Priority.Medium;
+
+    }
+}
